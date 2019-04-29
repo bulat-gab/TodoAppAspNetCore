d20965b00e4f8d3f50e3a002a3d30307629032e5
gabbr <gabbr@skbkontur.ru>
2019-04-29 19:02:40 +0300

    *

--- src/TodoApp/Startup.cs
+++ src/TodoApp/Startup.cs
@@ -33,7 +33,6 @@ namespace TodoApp
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
             var mongoSettings = new MongoSettings
